ec4cd326f58dfa5573d43bcf065d595c8b7a9d45
Daniel Pawlak <daniel@Daniels-iMac.local>
2020-12-11 14:37:53 -0800

    staticfiles_storage

--- api_project/settings.py
+++ api_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
